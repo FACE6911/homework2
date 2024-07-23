c63e0571257c04142f6c70828bfa7d0ccd172484
FACE6911 <boss.haydar@bk.ru>
2024-07-23 16:54:28 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_of_homeworks = 12
+count_of_hours = 1.5
+subjectName = 'Python,'
+timefordoing = count_of_hours / count_of_homeworks
+print(subjectName, 'всего задач:',count_of_homeworks,',' 'затрачено часов:', count_of_hours,',' 'среднее время выполнения',timefordoing, 'часа')
